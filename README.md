b8c572958656cf50495389b36650f84acaf8cba6
FigueiredoFelipe <111877813+FigueiredoFelipe@users.noreply.github.com>
2022-10-02 16:00:12 -0300

    New feature added play and pause
    Update 02/10/2022 - Newfeature - Added play and pause the song when click.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Testing some "image rotation" with CSS.
 Live demo: https://felipefigueiredo-tree.netlify.app/
 
 Update 02/10/2022 - Added Song.
+
+Update 02/10/2022 - Newfeature - Added play and pause the song when click.
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,14 @@
-window.addEventListener('click', () => {
-  document.getElementById('song').play();
+// window.addEventListener('click', () => {
+//   document.getElementById('song').play();
+// });
+
+let cb = document.querySelector('#cb');
+
+cb.addEventListener('click', (event) => {
+  console.log('val: ', cb.checked);
+  if (cb.checked) {
+    document.getElementById('song').play();
+  } else {
+    document.getElementById('song').pause();
+  }
 });
